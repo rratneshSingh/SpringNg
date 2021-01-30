d55f791cf4b24792a6a4ea7995f7f23722f2773d
ratnesh singh <dev.ratnesh.singh@gmail.com>
2021-01-30 19:38:40 +0530

    fundamentals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BMICalc';
+  url = 'http://raivens.com/wp-content/uploads/2016/08/Dummy-image.jpg';
+  classes = "c1 c2"
+  isC1 = true;
+  isC2 = true;
+  styles = "color: red;"
+  bgColor = "red";
+
+  changeColor( value ) {
+    
+  }
+  
+  showBgColorVaribaleValue() {
+    console.log(this.bgColor);
+  }
+
+  getInput( event ) {
+    console.log( event );
+  }
 }
