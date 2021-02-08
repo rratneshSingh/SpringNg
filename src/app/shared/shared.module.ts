525a78bb0e20360e668dac81c46a699ffc1409c7
ratnesh singh <dev.ratnesh.singh@gmail.com>
2021-02-08 23:14:46 +0530

    forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HighlightDirective } from './highlight.directive';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ SignUpComponent, HighlightDirective ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [ SignUpComponent, HighlightDirective ]
 })
--- src/app/shared/sign-up/sign-up.component.ts
+++ src/app/shared/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,14 +11,42 @@ export class SignUpComponent implements OnInit {
   @Input() data;
   @Output() onDataSelect = new EventEmitter();
 
+  signUpForm: FormGroup = null;
+
+  user = null;
+
   constructor() { 
   }
 
   ngOnInit(): void {
+    this.getUser();
+    this.signUpForm = new FormGroup({
+      name: new FormControl('',  [Validators.required] ),
+      email: new FormControl('', [ Validators.email, Validators.required ] ),
+      password: new FormControl('', [ Validators.required, Validators.minLength( 8 ) ] )
+    });
+    if ( this.user ) {
+      this.signUpForm.patchValue( this.user );
+      this.markAsTouched();
+    }
+  }
+
+  getUser() {
+    const user = JSON.parse( localStorage.getItem('signUpData') );
+    this.user = user;
+  }
+
+  markAsTouched(){
+    this.signUpForm.get('name').markAsTouched();
   }
 
   emitSelectedItem( item ) {
     this.onDataSelect.emit( item );
+  }
+
+  signUp() {
+    console.log(this.signUpForm.value);
+    localStorage.setItem('signUpData', JSON.stringify( this.signUpForm.value ) );
   }
 
 }
