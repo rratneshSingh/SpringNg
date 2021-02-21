03e5e8758ead2976525089fd251d22d13a793b50
ratnesh singh <dev.ratnesh.singh@gmail.com>
2021-02-21 16:33:31 +0530

    fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import {
-  DomSanitizer
-} from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,37 +6,4 @@ import {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'BMICalc';
-  url = 'http://raivens.com/wp-content/uploads/2016/08/Dummy-image.jpg';
-  classes = "c1 c2"
-  isC1 = true;
-  isC2 = true;
-  styles = "color: red;"
-  bgColor = "red";
-  arrayItems = [ 'bmw', 'maruti', 'ferrari'];
-  isSubmit = false;
-  switchValue = 2;
-  selectedArrayItem = null;
-
-  changeColor( value ) {
-    
-  }
-  
-  showBgColorVaribaleValue() {
-    console.log(this.bgColor);
-  }
-
-  getInput( event ) {
-    console.log( event );
-  }
-
-  show( value ){
-    this.isSubmit = true;
-    console.log( value );
-  }
-
-  constructor(public sanitization:DomSanitizer){
-  }
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SharedModule } from './shared/shared.module';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    HomeComponent,
-    AboutComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    SharedModule,
-    FormsModule,
-    RouterModule.forRoot(
-      [
-        { path: '', component: HomeComponent  },
-        { path: 'login', component: LoginComponent },
-        { path: 'about', component: AboutComponent }
-      ]
-    )
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
